9cc3adbed2fdbaa9306b179241310ee8a5cf29e3
Maria Lourdes Armenta Lindoro <maria.al@culiacan.tecnm.mx>
2023-10-04 13:30:05 -0600

    Actualizacion y cliente de clase Lista

--- EstructuradeDatosAgo-Dic2023/src/listas/Lista.java
+++ EstructuradeDatosAgo-Dic2023/src/listas/Lista.java
@@ -8,7 +8,7 @@ package listas;
  *
  */
 public class Lista<T> {
-	//Primer element o de la lista
+	//primer elemento de la lista
 	private Nodo<T> cabeza;
 	//Total de elementos de la lista
 	private int tamanio;
@@ -17,22 +17,23 @@ public class Lista<T> {
 		cabeza = null;
 		tamanio = 0;
 	}
+	//Devolver el tamanio de la lista
 	public int getTamanio() {
 		return tamanio;
 	}
-	/*
+	/**
 	 * Consulta si la lista esta vacia
 	 * @return true cuando esta vacia, false cuando no
 	 */
 	public boolean esVacia() {
-		if (cabeza == null)
+		if(cabeza==null)
 			return true;
 		else
-			return false;	
+			return false;
 	}
 	/**
-	 * Agrega un nuevo nodo al final de la lista
-	 * @param valor valor del elemento a agregar
+	 * Agregar un nuevo nodo AL FINAL de la lista
+	 * @param valor valo a agregar
 	 */
 	public void agregar(T valor) {
 		Nodo<T> nuevo = new Nodo<T>();
@@ -41,38 +42,115 @@ public class Lista<T> {
 			cabeza = nuevo;
 		}
 		else {
-			//Agregar al final de la lista
-			Nodo<T> aux =cabeza;
-			while (aux.getSiguiente() !=null) {
+			//agregar al final de la lista
+			Nodo<T> aux = cabeza;
+			while (aux.getSiguiente() != null) {
 				aux = aux.getSiguiente();
 			}
 			aux.setSiguiente(nuevo);
+			
 		}
 		tamanio++;
-		
-		
 	}
-	public void insertar(T valor, int pos)  throws PosicionIlegalException{
+	/**
+	 * inserta un nuevo nodo en la lista
+	 * @param valor valor a agregar
+	 * @param pos posicion donde se insertara el nodo
+	 * @throw PosicionIlegalException excepcion en caso que la 
+	 *    posición no exista
+	 */
+	public void insertar(T valor, int pos) throws PosicionIlegalException {
 		if (pos>=0 && pos<=tamanio) {
 			Nodo<T> nuevo = new Nodo<T>();
 			nuevo.setValor(valor);
-			if (pos == 0) {
+			//el nodo a insertar esta al comienzo
+			if (pos==0) {
+
 				nuevo.setSiguiente(cabeza);
-				cabeza = nuevo;
-			} else {
+				cabeza= nuevo;
+		
+			}else {
+				//El nodo a inserta va al final de la lista
+				if (pos==tamanio) {
+					Nodo<T> aux =cabeza;
+					while(aux.getSiguiente() != null) {
+						aux = aux.getSiguiente();
+					}
+					aux.setSiguiente(nuevo);
+				}
 				
+				//el nodo a insertar esta en medio
+				else {
+					Nodo<T> aux = cabeza;
+					for (int i=0;i<=pos-2;i++){
+						aux = aux.getSiguiente();
+					}
+					Nodo<T> siguiente = aux.getSiguiente();
+					aux.setSiguiente(nuevo);
+					nuevo.setSiguiente(siguiente);
+					
+				}
 			}
-			
-			
+			tamanio++;
 			
 		}
 		else {
 			throw new PosicionIlegalException();
+		}
+		
+	}
+	public T getValor(int pos) throws PosicionIlegalException{
+		if (pos>=0 && pos<tamanio) {
+			T valor;
+			if (pos==0) {
+				valor=cabeza.getValor();
+				return valor;
+			}
+			else {
+				Nodo<T> aux= cabeza;
+				for(int i=0;i<=pos-1;i++) {
+					aux=aux.getSiguiente();
+				}
+				valor = aux.getValor();
+			}
+			return valor;
 			
 		}
+		else
+		{
+			throw new PosicionIlegalException();
+		}
 	}
-	
-	
-	
+	/**
+	 * Elimina un nodo en una determinada posiciom
+	 * @param pos posicion
+	 * @throws PosicionIlegalException
+	 */
+	public void remover(int pos) throws PosicionIlegalException{
+		if(pos>=0 && pos<tamanio) {
+			if (pos==0) {
+				//El nodo a eliminar esta en la primera posicion
+				cabeza = cabeza.getSiguiente();
+				tamanio--;
+			}
+			else {
+				Nodo<T> aux = cabeza;
+				for(int i=0;i<=pos-2;i++){
+					aux = aux.getSiguiente();
+				}
+				Nodo<T>prox = aux.getSiguiente();
+				aux.setSiguiente(prox.getSiguiente());
+				tamanio--;	
+			}
+			
+		}
+		else {
+			throw new PosicionIlegalException();
+		}
+	}
+	public void limpiar() {
+		cabeza = null;
+		tamanio = 0;
+	}
 
 }
